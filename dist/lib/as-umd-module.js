27b937cd7b33177f06113e4e8b2ac5b124f2c74d
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2019-12-22 23:19:34 +0200

    Update build

--- dist/lib/as-umd-module.js
+++ dist/lib/as-umd-module.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var javascripttostring_1 = __importDefault(require("javascripttostring"));
+var javascripttostring_1 = __importDefault(require("@lopatnov/javascripttostring"));
 function asUmdModule() {
     var values = [];
     for (var _i = 0; _i < arguments.length; _i++) {
